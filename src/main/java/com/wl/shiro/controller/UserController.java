1291a447dccace0796298f950cc9101775076e43
wlong <wanglong@ysstech.com>
2020-01-03 09:34:56 +0800

    springboot+shiro Demo

---
+++ src/main/java/com/wl/shiro/controller/UserController.java
@@ -0,0 +1,64 @@
+package com.wl.shiro.controller;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/toIndex")
+    public String index(Model model) {
+        model.addAttribute("name", "wl");
+        return "index";
+    }
+
+    @RequestMapping("/add")
+    public String add() {
+        return "/user/add";
+    }
+
+    @RequestMapping("/update")
+    public String update() {
+        return "/user/update";
+    }
+
+    @RequestMapping("/toLogin")
+    public String toLogin() {
+        return "/login";
+    }
+
+    @RequestMapping("/noAuth")
+    public String noAuth() {
+        return "/noAuth";
+    }
+
+    /**
+     * 登录逻辑处理
+     */
+    @RequestMapping("/login")
+    public String login(String name, String password, Model model) {
+        //1.获取Subject
+        Subject subject = SecurityUtils.getSubject();
+        //2.封装用户数据
+        UsernamePasswordToken token = new UsernamePasswordToken(name, password);
+        //3.执行登录方法
+        try {
+            subject.login(token);
+            return "redirect:/toIndex";
+        } catch (UnknownAccountException e) {
+            model.addAttribute("msg", "用户名不存在");
+            return "login";
+        } catch (IncorrectCredentialsException e) {
+            model.addAttribute("msg", "密码错误");
+            return "login";
+        }
+    }
+
+
+}
